bbd7b861c1beb876c4c14f2b76f1153cdce5b199
Patryk Jagielski <patryk1993xd@gmail.com>
2021-03-16 20:28:11 +0100

    falke8 isort

--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
-from .models import Product, Category
+from .models import Category, Product
 
 
 def categories(request):
