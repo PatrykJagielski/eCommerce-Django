aceb7c10df855cb1472f99e201dbb12817c61e96
Patryk Jagielski <patryk1993xd@gmail.com>
2021-03-18 18:05:35 +0100

    add item to basket

--- basket/basket.py
+++ basket/basket.py
@@ -1,4 +1,6 @@
+from decimal import Decimal
 
+from store.models import Product
 
 
 class Basket():
@@ -10,5 +12,39 @@ class Basket():
         self.session = request.session
         basket = self.session.get('skey')
         if 'skey' not in request.session:
-            basket = self.session['skey'] = {'number': 1231231}
+            basket = self.session['skey'] = {}
         self.basket = basket
+
+    def add(self, product, qty):
+        """
+        Adding and updating the user basket session data
+        """
+        product_id = product.id
+
+        if product_id not in self.basket:
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(product_qty)}
+
+        self.session.modified = True
+
+    def __iter__(self):
+        """
+        Collect the product_id in the session data to query the database
+        and return products
+        """
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
+
+    def __len__(self):
+        """
+        Get the basket data and count quantity of items
+        """
+        return sum(item['qty'] for item in self.basket.values())
