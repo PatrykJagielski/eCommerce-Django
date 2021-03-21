6206e02fca3d1a0d0779b8606fcebf0686c7ee20
Patryk Jagielski <patryk1993xd@gmail.com>
2021-03-21 21:37:08 +0100

    build registration template

--- account/views.py
+++ account/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from .forms import RegistrationForm
+
+
+def account_register(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+
+    if request.method == 'POST':
+        registerForm = RegistrationForm(request.POST)
+        if registerForm.is_valid():
+            user = registerForm.save(commit=True)
+            user.email = registerForm.cleaned_data['email']
+            user.set_password(registerForm.cleaned_data['password'])
+            user.is_active = False
+            user.save()
+            # Setup email
+            current_site = get_current_site(request)
+            subject = 'Activate your Account'
+            message = render_to_string('account/registration/account_activation_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_registrarion_token.make_token(user)
+            })
+            user.email_user(subject=subject, message=message)
+        else:
+            registerForm = RegistrationForm()
+        return render(request, 'account/registration/register.html', {'form': registerForm})
