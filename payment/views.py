3831975bdd901a17dbc25b0755ea18733ce99f9c
Patryk Jagielski <patryk1993xd@gmail.com>
2021-03-27 14:59:31 +0100

    setup stripe

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,24 @@
+import stripe
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+
+from basket.basket import Basket
 
 
 @login_required
 def BasketView(request):
-     return render(request, 'payment/home.html')
+
+    basket = Basket(request)
+    total = str(basket.get_total_price())
+    total = total.replace('.', '')
+    total = int(total)
+
+    stripe.api_key = 'sk_test_51IZc1lIufSawS3O8UOQIii1mCyuMgDzcZT2azj1M7balJsTqyYhNWPAm7CpwG2sr28yeR9eoKIIQL8qg369Dveou00z992GmB3'
+    intent = stripe.PaymentIntent.create(
+        amount=total,
+        currency='gbp',
+        metadata={'userid': request.user.id}
+    )
+
+    return render(request, 'payment/home.html', {'client_secret': intent.client_secret})
